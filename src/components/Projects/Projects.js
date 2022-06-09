25909b17d6f7d9f5ee011a649871af4944c6d5a5
Afridi Azmain <Azmain951>
2022-06-09 17:10:01 +0600

    project deatils added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,8 @@ import React from 'react';
 import project1 from '../../images/project1.png';
 import project2 from '../../images/project2.png';
 import project3 from '../../images/project3.png';
+import { GrReactjs, GrNode } from "react-icons/gr";
+import { SiBootstrap, SiTailwindcss, SiMongodb, SiHtml5, SiCss3 } from "react-icons/si";
 
 const Projects = () => {
     return (
@@ -19,6 +21,17 @@ const Projects = () => {
                         <p className='text-justify'>Option for admin to add and delete questions in different exam categories.</p>
                         <p className='text-justify'>Profile, Analytics and Leaderboard page to show exam records and track progress.</p>
                         <hr />
+                        <div>
+                            <p className='uppercase text-center font-bold text-accent'>Technology Used</p>
+                            <div className='flex justify-evenly items-center p-5 text-primary text-3xl'>
+                                <SiHtml5></SiHtml5>
+                                <SiCss3></SiCss3>
+                                <SiBootstrap></SiBootstrap>
+                                <GrReactjs></GrReactjs>
+                                <GrNode></GrNode>
+                                <SiMongodb></SiMongodb>
+                            </div>
+                        </div>
                         <div class="card-actions justify-around mt-5">
                             <a class="btn btn-sm btn-outline btn-accent" href='https://examprep-1edff.web.app/' target='_blank'>Client</a>
                             <a class="btn btn-sm btn-outline btn-accent" href='https://examprep-1edff.web.app/' target='_blank'>Website</a>
@@ -39,6 +52,14 @@ const Projects = () => {
                         <hr />
                         <div>
                             <p className='uppercase text-center font-bold text-accent'>Technology Used</p>
+                            <div className='flex justify-evenly items-center p-5 text-primary text-3xl'>
+                                <SiHtml5></SiHtml5>
+                                <SiCss3></SiCss3>
+                                <SiTailwindcss></SiTailwindcss>
+                                <GrReactjs></GrReactjs>
+                                <GrNode></GrNode>
+                                <SiMongodb></SiMongodb>
+                            </div>
                         </div>
                         <div class="card-actions justify-around mt-5">
                             <a class="btn btn-sm btn-outline btn-accent" href='https://manufacturer-website-6cbb8.web.app/' target='_blank'>Client</a>
@@ -59,6 +80,17 @@ const Projects = () => {
                         <p className='text-justify'>Option for users to restock fruits and delivery option.</p>
                         <p className='text-justify'>Secure API to show user's added items on my items page.</p>
                         <hr />
+                        <div>
+                            <p className='uppercase text-center font-bold text-accent'>Technology Used</p>
+                            <div className='flex justify-evenly items-center p-5 text-primary text-3xl'>
+                                <SiHtml5></SiHtml5>
+                                <SiCss3></SiCss3>
+                                <SiBootstrap></SiBootstrap>
+                                <GrReactjs></GrReactjs>
+                                <GrNode></GrNode>
+                                <SiMongodb></SiMongodb>
+                            </div>
+                        </div>
                         <div class="card-actions justify-between mt-5">
                             <a class="btn btn-sm btn-outline btn-accent" href='https://fruit-basket-523c2.web.app/' target='_blank'>Client</a>
                             <a class="btn btn-sm btn-outline btn-accent" href='https://fruit-basket-523c2.web.app/' target='_blank'>Website</a>
